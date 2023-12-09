3b336e900248269862bceeaf9aab0784a06e92ee
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-12-09 17:54:47 +0700

    feat: 🎸 working with fragment

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,12 @@ function App() {
   }
 
   return (
-    <div>
+    /* In React, the `<Fragment>` component is used to group multiple elements together without adding
+    an extra DOM element. It is a way to return multiple elements from a component's render method
+    without wrapping them in a parent element. This is useful when you don't want to introduce
+    unnecessary divs or other elements into the DOM structure. */
+    // <Fragment> </Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -67,7 +72,7 @@ function App() {
           </div>
         )}
       </main>
-    </div>
+    </>
   );
 }
 
