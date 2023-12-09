6b2950caae1de669b4cd71eabd6207c8e91e5552
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-12-09 22:34:38 +0700

    fix: 🐛 state not use in app.jsx but in Section Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 import Examples from "./components/Examples";
-import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-  }
-
   return (
     /* In React, the `<Fragment>` component is used to group multiple elements together without adding
     an extra DOM element. It is a way to return multiple elements from a component's render method
